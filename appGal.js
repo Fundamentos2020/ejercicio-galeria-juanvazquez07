d4e435c1fdd6b0e5045cdb9289ce7ac774bbd2f8
juanvazquez07 <juancarlosvazquezgarcia07@gmail.com>
2020-03-27 23:28:37 -0600

    Add files via upload
    paginacion para atras

--- appGal.js
+++ appGal.js
@@ -68,21 +68,18 @@ var sig = document.getElementById('flecha2').addEventListener('click', moverD);
 
 function moverI(e){
     e.preventDefault();
-    console.log(cont);
-    /*const cantidad = document.getElementById('numero').value;
-    const num = 100/cantidad;
-
-    let url = '';
-
-    url += 'https://picsum.photos/v2/list?page=';
-    url += num;
-    url += '&limit='
-    url += cantidad;
-
+    console.log("clic flecha izquierda");
+    
+    console.log(Url);
+    var p = parseInt(cont, 10);
+    if(p > 0){
+         //console.log(cont);
+    var s = parseInt(canPg, 10);
+    let n = p-s;
+    var i= n-s;
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', url, true);
-    console.log(url);
+    xhr.open('GET', Url, true);
     xhr.onload = function(){
        if(this.status === 200){
            //console.log(this.responseText);
@@ -90,6 +87,24 @@ function moverI(e){
 
            let contenido = '';
            let nurl = '';
+           console.log('limite'+n);
+           console.log('inicio'+p);
+           for ( i; i<n;i++){
+            //console.log(imagen[i]); 
+            console.log(i); 
+            nurl += 'https://picsum.photos/id/';
+            nurl += `${imagen[i].id}`;
+            nurl += '/3504/2336'
+            contenido += `
+            <div class="contenedor overflow-hidden" id"boton">
+               <img src="${nurl}" alt="" class="img"></img>
+            </div>`;
+            nurl = '';
+            }
+            cont = i;
+            console.log('indicef'+i);
+            document.getElementById('resultado').innerHTML = contenido;
+           /*
            imagen.forEach(function(info) {
             nurl += 'https://picsum.photos/id/';
             nurl += `${info.id}`;
@@ -101,21 +116,12 @@ function moverI(e){
             nurl = '';
             //console.log(contenido);
            })
-         document.getElementById('resultado').innerHTML = contenido;
-        
-         let pag = '';
-         for (var i=0; i<num;i++){
-         pag += `
-           <div class="contenedor c-pag ">
-             <a href="" id="boton">${i+1}</a>
-           </div>`;
-         }
-         //console.log(pag);
-          document.getElementById('paginacio').innerHTML = pag;
+           document.getElementById('resultado').innerHTML = contenido;*/
        }
     }
     
-    xhr.send();*/
+    xhr.send();
+    }
     
 }
 
